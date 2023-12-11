a5aa0067df4c8af1516317eb9cf777526ec21754
scgines <giness@uci.edu>
2023-12-10 17:09:20 -0800

    pitchfork is there but not functional

---
+++ Assets/PreSpawnedPitchfork.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreSpawnedPitchforkPitchfork : PreSpawnedItem
+{
+    public override ItemData SpawnItem => new Pitchfork();
+}
