63b96236fd6e15cec63d77a4821a0d3fe3b7cd08
Vortex <Thederpsnail@gmail.com>
2023-12-10 21:53:04 -0800

    corn eating sound fx

--- Assets/Zombie.cs
+++ Assets/Zombie.cs
@@ -41,8 +41,8 @@ public class Zombie : EntityWithCharDrawing
     [SerializeField] private float chanceForPotatoGun = 0.0025f;
     public void AssignItem(ref ItemData item)
     {
-        float difficulty = EnemyScalingFactor; //starts at 1, becomes 3 at 15 minutes. Linear scaling
-        if(chanceForCorn * difficulty > Random.Range(0, 1f))
+        float difficulty = 1 - EnemyScalingFactor; //starts at 1, Linear scaling as time progresses. Until it reaches 2, at which sqrt scaling begins
+        if(chanceForCorn * (1 + difficulty) > Random.Range(0, 1f))
         {
             item = new Corn();
         }
